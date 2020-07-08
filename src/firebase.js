0cfff83bdd4e84608ea51399fc8d8a0e54484569
Vitoko4k <vicsan2013@gmail.com>
2020-07-08 00:54:21 -0400

    base de datos vinculada

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDF1bV8l8k2SvZF_NYJo3i8d-5WKLC0QVA",
+    authDomain: "auth-229d2.firebaseapp.com",
+    databaseURL: "https://auth-229d2.firebaseio.com",
+    projectId: "auth-229d2",
+    storageBucket: "auth-229d2.appspot.com",
+    messagingSenderId: "37893473529",
+    appId: "1:37893473529:web:86ae2ee8212deed74bce28"
+  };
